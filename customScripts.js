3683166ac5932785c4ca84917218224bbda7d2df
Tiffany <tiffmollica@gmail.com>
2021-05-12 17:12:18 -0400

    updated some styles for bullets and paragraphs

---
+++ customScripts.js
@@ -0,0 +1,7 @@
+// typing text animation script
+var typed = new Typed('.typing', {
+  strings: ['Designer - Developer - Dreamer :)', 'Blood Type: B Positive  :)'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
--- scripts.js
+++ scripts.js
@@ -70,12 +70,3 @@
     });
 
 })(jQuery); // End of use strict
-
-
-// typing text animation script
-    var typed = new Typed('.typing', {
-      strings: ['Designer - Developer - Dreamer :)', 'Blood Type: B Positive  :)'],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
